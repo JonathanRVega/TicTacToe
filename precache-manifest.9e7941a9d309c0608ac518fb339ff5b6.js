412915132b15e7080961b3487ff8c2687355f40f
JonathanRVega <Jonathaan.rv@gmail.com>
2020-10-08 10:04:19 -0600

    Updates

---
+++ precache-manifest.9e7941a9d309c0608ac518fb339ff5b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab46965d69f303f1ef12ebd56fa704a",
+    "url": "/%7BTicTacToe%7D/index.html"
+  },
+  {
+    "revision": "af39866b6c2450cc45da",
+    "url": "/%7BTicTacToe%7D/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "43db37308c4d8dc94247",
+    "url": "/%7BTicTacToe%7D/static/js/2.8bdd3a2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7BTicTacToe%7D/static/js/2.8bdd3a2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af39866b6c2450cc45da",
+    "url": "/%7BTicTacToe%7D/static/js/main.f9061f3d.chunk.js"
+  },
+  {
+    "revision": "83aece9abf9fa323f0c9",
+    "url": "/%7BTicTacToe%7D/static/js/runtime-main.1eb2654d.js"
+  }
+]);
